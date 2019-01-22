eb5fbc9281c730c2d2fc3dc42ca67ddf44eec7b3
Jeff Jetton <jeff.jetton@gmail.com>
2019-01-21 21:29:57 -0600

    Update porting notes

--- aceyducey.py
+++ aceyducey.py
@@ -7,7 +7,7 @@
 #
 # "This is a simulation of the Acey Ducey card game.
 #  In the game, the dealer (the computer) deals two
-#  cards face up. You have an option to bet or not to
+#  cards face up.  You have an option to bet or not to
 #  bet depending on whether or not you feel the next
 #  card dealt will have a value between the first two.
 #
@@ -18,7 +18,7 @@
 # "The original BASIC program author was Bill Palmby
 #  of Prairie View, Illinois."
 #
-# Python port by Jeff Jetton
+# Python port by Jeff Jetton, 2019
 #
 ########################################################
 
@@ -65,13 +65,13 @@ while keep_playing:
     bankroll = DEFAULT_BANKROLL
     display_bankroll(bankroll)
 
-    # Loop for a single round. Repeat until out of money.
+    # Loop for a single round.  Repeat until out of money.
     while bankroll > 0:
 
         # Deal out dealer cards
         print("Here are your next two cards")
         dealer1 = deal_card_num()
-        # If the cards match, we re-deal 2nd card until they don't
+        # If the cards match, we redeal 2nd card until they don't
         dealer2 = dealer1
         while dealer1 == dealer2:
             dealer2 = deal_card_num()
@@ -133,55 +133,42 @@ print("OK Hope you had fun\n")
 #   Maybe it did something in feature that was edited
 #   out of the final version used in the book?
 #
-#   The card value printing code was originally
-#   repeated three times: Once for the two dealer
-#   cards and again for the player card. This has
-#   been broken out into a single, reused function.
-#
 #   The original program simply generated random numbers
-#   for each card. It did not simulate a true card deck,
+#   for each card.  It did not simulate a true card deck,
 #   where the dealing of a card eliminates it from the
 #   deck and reduces the chances of the same value
-#   being drawn. This "infinite deck" logic (or "deal,
+#   being drawn.  This "infinite deck" logic (or "deal,
 #   with replacement after") has NOT been changed.
 #
 #   Like the original program, we still allow entering
 #   negative bet values, which will earn the player
 #   money if they lose! :-)
 #
-#   The original program tracked cards as integers that
-#   directly matched their values. So a deuce was 2, etc.
-#   Here, cards are internally represented by integers
-#   between 0 and 12. A deuce is now stored as 0, and
-#   merely displayed to the player as "2". This does
-#   not affect the calculation of wins/losses, but it
-#   does make translating the number to text a bit
-#   more straightforward.
-#
 #
 # Ideas for Modifications
 #
 #   Give the user the ability to quit the game, perhaps
-#   by typing "quit" instead of making a bet. Provide a
+#   by typing "quit" instead of making a bet.  Provide a
 #   final assement based on how much of the original
 #   bankroll they have left.
 #
 #   Or have the game run for a set number of rounds or
 #   until a certain bankroll goal is attained.
 #
+#   Implement an "ante"--a set amount the player has to
+#   bet each time rather than having the option to lay
+#   out entirely.
+#
 #   See "porting notes" above about negative bet values.
 #   In fact, other than checking to make sure the player
 #   has enough in the bankroll, there is no validation
-#   of bet input. Try entering a word instead of a
-#   number, for example. What happens? How would you
+#   of bet input.  Try entering a word instead of a
+#   number, for example.  What happens?  How would you
 #   fix these problems?
 #
 #   When the player "chickens out", show them what the
 #   next card would've been and point out whether they
 #   made a good or bad decision.
-# 
-#   Instead of calling the player "chicken" every time
-#   they opt out of betting, how about random taunts?
 #
 #   In what situations are the odds of winning high
 #   enough to justify making a bet? Create a cheat mode
